14976fa2fb0201297f4e64ddb394a7e834204e22
Ari Jort <arijort@gmail.com>
2022-12-16 13:42:35 -0500

    code improvements for part 2

--- day08/day8.py
+++ day08/day8.py
@@ -3,8 +3,6 @@
 import sys
 from pprint import pprint
 from enum import Enum
-#from functools import reduce
-import itertools
 import math
 import numpy as np
 filename = "input.txt"
@@ -34,88 +32,20 @@ input is 99x99 grid
 """
 
 def check_scenic(row,col,graph, scenic, left,up,down,right):
-  #check each direction
+  # check each direction
   for direction, idx in zip([left, right, up, down], [TreeVis.LEFT, TreeVis.RIGHT, TreeVis.TOP, TreeVis.BOTTOM]):
-    max_hgt, tree_count = -1, 0
+    tree_count = 0
     for tree in direction:
-      if int(tree) > max_hgt:
-        tree_count += 1
-        max_hgt = max(max_hgt, int(tree))
-      else:
+      tree_count += 1
+      if tree >= graph[row][col]:
         break
     scenic[row][col][idx.value] = tree_count
-    return scenic
-  #for r in range(row-1,-1,-1): # if row is 0 this loop will be empty
-  #  if int(graph[r][col]) > max_hgt:
-  #    tree_count += 1
-  #    max_hgt = max(max_hgt, int(graph[r][col]))
-  #  else:
-  #    break
-  #scenic[row][col][TreeVis.TOP.value] = tree_count
-
-  #check downwards
-def not_called():
-  max_hgt, tree_count = -1, 0
-  for r in range(row+1, len(graph)): # if row is 0 this loop will be empty
-    if int(graph[r][col]) > max_hgt:
-      tree_count += 1
-      max_hgt = max(max_hgt, int(graph[r][col]))
-    else:
-      break
-  scenic[row][col][TreeVis.BOTTOM.value] = tree_count
-
-  # check left
-  max_hgt, tree_count = -1, 0
-  for c in range(col-1, -1, -1):
-    if int(graph[row][c]) > max_hgt:
-      tree_count += 1
-      max_hgt = max(max_hgt, int(graph[row][c]))
-    else:
-      break
-  scenic[row][col][TreeVis.LEFT.value] = tree_count
-
-  # check right
-  max_hgt, tree_count = -1, 0
-  for c in range(col+1,len(graph[0])):
-    if int(graph[row][c]) > max_hgt:
-      tree_count += 1
-      max_hgt = max(max_hgt, int(graph[row][c]))
-    else:
-      break
-  scenic[row][col][TreeVis.RIGHT.value] = tree_count
+    #print(f"  got tree count {tree_count} going to {idx}")
   return scenic
 
 def check_cell(row,col,graph,vis, left, up, down, right):
   for direction, idx in zip([left, right, up, down], [TreeVis.LEFT, TreeVis.RIGHT, TreeVis.TOP, TreeVis.BOTTOM]):
-    vis[row][col][idx.value] = all( [ t < graph[row][col] for t in direction ] )
-    #for t in direction:
-    #  if t >= graph[row][col]:
-    #    vis[row][col][idx.value] = False
-    #    break
-  return vis
-
-def check_cell_old(row,col,graph,vis):
-  #check top
-  for r in range(row):
-    if graph[r][col] >= graph[row][col]:
-      vis[row][col][TreeVis.TOP.value] = False
-      break
-  # check bottom
-  for r in range(row+1,len(graph)):
-    if graph[r][col] >= graph[row][col]:
-      vis[row][col][TreeVis.BOTTOM.value] = False
-      break
-
-  # check left
-  for c in range(col):
-    if graph[row][c] >= graph[row][col]:
-      vis[row][col][TreeVis.LEFT.value] = False
-      break
-  # check right
-  for c in range(col+1,len(graph[0])):
-    if graph[row][c] >= graph[row][col]:
-      vis[row][col][TreeVis.RIGHT.value] = False
-      break
+    vis[row][col][idx.value] = all( [ t < graph[row][col] for t in direction ] ) # check all trees are lower than focal point
   return vis
 
 def main():
@@ -128,40 +58,23 @@ def main():
   scenic = [[[ 0 for _ in range(len(TreeVis))] for _ in range(len(mat[0])) ] for _ in  range(len(mat)) ]
   npmat = np.array(mat)
  
-  # exclude edges
+  # exclude edges; start with 1
   for r in range(1,len(mat)-1) :
     for c in range(1,len(mat[0]) - 1):
 
-      left  = np.flip(npmat[r, 0:c ])
-      up    = np.flip(npmat[0:r, c ])
-      right =        npmat[r, c+1:]
-      down  =        npmat[r+1:, c]
+      left  = np.flip(npmat[r,   0:c ])
+      up    = np.flip(npmat[0:r, c   ])
+      right =         npmat[r,   c+1:]
+      down  =         npmat[r+1:,c   ]
 
-      vis    = check_cell(r,c,mat,vis, left, up, down, right)
-
-      print(f"checking {r},{c}")
-      scenic = check_scenic(r,c,mat,scenic, left,up,down,right)
-      #print(f"scenic {scenic}")
-
-      #up   =  [ uprow[c]   for uprow   in mat[r-1:-1:-1] ]
-      #down =  [ downrow[c] for downrow in mat[r:]        ]
-      #left  = [ leftcol[c-1:-1:-1] for leftcol in mat[r]       ] 
-      #right = [ rghtcol[c+1:] for rghtcol in mat[r]      ] 
-      print(f"r {r} c {c} up is    {type(up)} {up} ")
-      print(f"r {r} c {c} down is  {type(down)} {down}")
-      print(f"r {r} c {c} left is  {type(left)} {left}")
-      print(f"r {r} c {c} right is {type(right)} {right}")
+      vis    = check_cell(  r,c,mat,vis,    left, up, down, right)
+      scenic = check_scenic(r,c,mat,scenic, left, up, down, right)
 
   result = sum([ any(c) for r in vis for c in r ])
-  npmat = np.array(mat)
-  #print(f"mp slice {npmat[0:2, 46:48]}")
   print (f"part 1 result len is {result}")
-  pprint(scenic)
-  tmp = [ math.prod(c) for r in scenic for c in r ]
-  print(len(tmp))
-  result2 = max(tmp)
+  result2 = max( [ math.prod(c) for r in scenic for c in r ] )
   #result2 = max([ reduce(lambda: x, y: x * y, c) for r in scenic for c in r ])
-  print (f"part 2 result is {result2}") # first guess 1026: wrong
+  print (f"part 2 result is {result2}") # should be 410400
 
 if __name__ == "__main__":
   main()
