5f657aba02b9d14b6c936ec9e6832c7bd745fe8f
Ari Jort <arijort@gmail.com>
2022-12-25 22:21:13 -0500

    updated dfs implementation

--- day12/day12.py
+++ day12/day12.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3.11
-
 import sys
+import numpy as np
 from pprint import pprint
 filename = "input.txt"
 #filename = "small"
@@ -17,91 +17,94 @@ Part 1:
   acctuvwj
   abdefghi
 
-
 Part 2
 
 Find shortest path from any 'a' elevation sqaure to the end.
 
 """
 
-
 grid = [] 
-costs = []
+heights = {}
+costs = np.array(0)
 
-climb_test = lambda ch, px, py: ord(ch) <= ord( grid[px][py]) + 1
+climb_test   = lambda h, px, py: h <= heights[(px,py)] + 1
+descend_test = lambda h, px, py: h >= heights[(px,py)] - 1
 
+dirs = [ (1,0), (-1,0), (0,1), (0,-1) ]
 
-def get_neighbors(px, py):
+def get_neighbors(px, py, move_test):
   neighbors = []
-  #print(f"getting neighbors from {px},{py}")
-  if px-1 >= 0 and ord(grid[px-1][py]) <= ord( grid[px][py] ) + 1:
-    #print(f"  checking {px},{py} up")
-    #print(f"  compared point {grid[px][py]} to neighbor {grid[px-1][py]}")
-    neighbors.append( (px-1, py) )
-  if px+1 < len(grid) and ord(grid[px+1][py]) <= ord( grid[px][py] ) + 1:
-    #print(f"  checking {px},{py} down")
-    #print(f"  compared point {grid[px][py]} to neighbor {grid[px+1][py]}")
-    neighbors.append( (px+1, py) )
-  if py-1 >= 0 and ord(grid[px][py-1]) <= ord( grid[px][py] ) + 1:
-    #print(f"  checking {px},{py} left")
-    #print(f"  compared point {grid[px][py]} to neighbor {grid[px][py-1]}")
-    neighbors.append( (px, py-1) )
-  if py+1 < len(grid[0]) and ord(grid[px][py+1]) <= ord( grid[px][py] ) + 1:
-    #print(f"  checking {px},{py} right")
-    #print(f"  compared point {grid[px][py]} to neighbor {grid[px][py+1]}")
-    neighbors.append( (px, py+1) )
-
-  #print(f"returning neighbors {neighbors}")
+  for dr, dc in dirs:
+    if 0 <= px + dr < len(grid) and 0 <= py + dc < len(grid[0]):
+      if move_test( heights[ (px+dr, py+dc) ], px, py):
+        neighbors.append( (px+dr, py+dc) )
   return neighbors
 
+def explore_any_startpoints(startr, startc, endr, endc, grid, move_test):
+  dfs = []
+  dfs.append( (startr,startc,0) )
+  startpoints = set()
+  while dfs:
+    pointr,pointc,cost = dfs.pop()
+    if grid[pointr][pointc] == 'a':
+      costs[pointr][pointc] = min(costs[pointr][pointc], cost)
+      startpoints.add( (pointr,pointc) )
+      continue
+    if costs[pointr][pointc] <= cost:
+      continue # ignore this point, already visited
+    costs[pointr][pointc] = min(costs[pointr][pointc], cost)
+    for pr,pc in get_neighbors(pointr,pointc, move_test):
+      if costs[pr][pc] <= cost + 1:
+        continue
+      dfs.append( (pr,pc,cost+1) )
+  return startpoints
 
-def explore(startr, startc, endr, endc, grid):
+def explore(startr, startc, endr, endc, grid, move_test):
   dfs = [] # stack of points for depth first search
   dfs.append( (startr, startc, 0) ) # initial cost
 
   while len(dfs) > 0:
     pointr, pointc, cost = dfs.pop()
-    print(f"exploring from {pointr},{pointc} with cost {cost}")
+    #print(f"exploring from {pointr},{pointc} with cost {cost}")
     if pointr == endr and pointc == endc:
       costs[endr][endc] = cost
       continue
     if costs[pointr][pointc] <= cost:
       continue # ignore this point, already visited
     costs[pointr][pointc] = min(costs[pointr][pointc], cost)
-    for pr,pc in get_neighbors(pointr, pointc):
+    for pr,pc in get_neighbors(pointr, pointc, move_test):
       if costs[pr][pc] <= cost+1:
         continue
-      #print(f"  pushing to stack {pr},{pc} with cost {cost+1}")
       dfs.append( (pr, pc, cost+1) )
-      #print(f"  stack is {dfs}")
-  
+
+def parse():
+  with open(filename, "r") as fh:
+    grid = [ list(line.strip()) for line in fh.readlines() ]
+  return grid
 
 def main():
-  #parse()
+  global grid,costs
+  grid = parse()
+  for x, row in enumerate(grid):
+    for y, ch in enumerate(row):
+      match ch:
+        case 'S':
+          start = (x, y)
+          ch = 'a'
+        case 'E':
+          end = (x, y)
+          ch = 'z'
+      heights[ (x,y) ] = ord(ch)
 
-  with open(filename, "r") as fh:
-    r = 0
-    for line in fh:
-      grid.append(list(line.strip()))
-      costline = [sys.maxsize] * (len(line)  - 1)
-      costs.append(costline)
-      c = 0
-      for ch in line:
-        if ch == 'S':
-          start = (r,c)
-        elif ch == 'E':
-          end = (r,c)
-        c += 1        
-      r += 1
-  #pprint(grid)
-  #print(f"have start {start} end {end}") 
+  costs = np.zeros(shape=( len(grid), len(grid[0]))) + sys.maxsize
+  explore(start[0], start[1], end[0], end[1], grid, climb_test)
+  r1 = costs[end[0]][end[1]] 
+  costs = np.zeros(shape=( len(grid), len(grid[0]))) + sys.maxsize
+  startpoints = explore_any_startpoints (end[0], end[1], start[0], start[1], grid, descend_test)
+  min_startpoints = min([ costs[pr][pc] for pr,pc in startpoints ])
 
-  grid[start[0]][start[1]] = 'a'
-  #costs[start[0]][start[1]] = 0
-  grid[end[0]][end[1]] = 'z'
-  explore(start[0], start[1], end[0], end[1], grid)
-  print(f"have travel cost {costs[end[0]][end[1]] }")
-  #pprint(costs)
+  print(f"part 1 travel cost {r1}")
+  print(f"part 2 travel cost {min_startpoints}")
 
 if __name__ == "__main__":
   main()
